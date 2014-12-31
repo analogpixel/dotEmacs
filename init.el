994bdfd635c5515915386f0733a9dab5c96442f7
Analog <analog@analogpixel.org>
2014-12-30 21:38:11 -0700

    new snipits, new modes

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; create a macro that allow setting anonymous functions easier
+;; expands to (lambda () (interactive) (command))
+(defmacro ## (var) (list 'lambda '() '(interactive) var))
+
 ;; load the package system so we can get new packages
 (require 'package)
 (add-to-list 'package-archives  '("melpa" . "http://melpa.org/packages/") t)
@@ -9,19 +13,27 @@
 				      (package-refresh-contents)
 				      (package-install lib) )))
 
+;; Various keyboard bindings
+(global-set-key (kbd "<end>")   (## (end-of-buffer)))
+(global-set-key (kbd "<home>")  (## (beginning-of-buffer)))
+(global-set-key (kbd "C-s")     (## (save-buffer)))
+(global-set-key (kbd "C-x C-s") (## (isearch-forward)))
+(global-set-key (kbd "<f12>")  'bookmark-jump)
+(global-set-key (kbd "<f11>")  'bookmark-set)
+(global-set-key (kbd "<f1>")   'flyspell-buffer)
+
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (wombat)))
  '(inhibit-startup-screen t)
  '(tool-bar-mode nil)
  '(truncate-lines nil)
- '(speedbar-show-unkown-files t)
- '(speedbar-update-flag nil)
  )
 
+;; set the windows size
+(when window-system (set-frame-size (selected-frame) 150 50))
+
+;; show me where my parens don't add up
+(show-paren-mode t)
 
 ;; active Babel languages
 (org-babel-do-load-languages
@@ -51,8 +63,6 @@
   "ace-jump-mode"
   "Emacs quick move minor mode"
   t)
-;; you can select the key you prefer to
-;;(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 (define-key global-map (kbd "C-1") 'ace-jump-mode)
 (define-key global-map (kbd "C-2") 'ace-jump-line-mode)
 
@@ -62,47 +72,7 @@
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
-
-;; quick run
-;; run the contents of the buffer
-;; https://github.com/syohex/emacs-quickrun
-(require 'quickrun)
-
-(quickrun-add-command "python"
-                      '((:command . "python")
-                        (:exec    . "%c %s"))
-                      :mode 'python-mode)
-
-(global-set-key (kbd "<f12>") 'quickrun)
-
-(global-set-key (kbd "<f1>") 'speedbar-get-focus)
-
-(speedbar)
-(speedbar-toggle-show-all-files)
-
-;; set common speedbar paths, maybe save some typing
-(defun sb-sk (k)
-  (setq default-directory k)
-  (speedbar-update-contents)
-  (speedbar-get-focus)
-  )
-
-(global-set-key (kbd "C-c C-1") (lambda () (interactive) (sb-sk "c:/users/analog/Desktop/") ))
-(global-set-key (kbd "C-c C-2") (lambda () (interactive) (sb-sk "c:/users/analog/Dropbox/") ))
-
-;; turn capslock into ctrl
-;; install AutoHotkey
-;; Save this to script.ahk
-;; double click to install
-;;
-;; #IfWinActive emacs  ; if in emacs
-;;    +Capslock::Capslock ; make shift+Caps-Lock the Caps Lock toggle
-;;    Capslock::Control   ; make Caps Lock the control button
-;;    #IfWinActive        ; end if in emacs
-
-
-
-;; enable snipits
+;; enable snippets
 ;; https://github.com/capitaomorte/yasnippet
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets/"))
@@ -113,18 +83,53 @@
 ;; http://www.johndcook.com/blog/emacs_windows/#aspell
 ;; http://aspell.net/win32/
 (setq-default ispell-program-name "C:/progra~2/Aspell/bin/aspell.exe")
-;;(setq text-mode-hook '(lambda() (flyspell-mode t) ))
 
-;; create a macro that allow setting anonymous functions easier
-;; expands to (lambda () (interactive) (command))
-(defmacro ## (var) (list 'lambda '() '(interactive) var))
+;; http://tuhdo.github.io/helm-intro.html
+(helm-mode t)
+(global-set-key (kbd "M-x")         'helm-M-x)
+(global-set-key (kbd "M-y")         'helm-show-kill-ring)
+(global-set-key (kbd "C-x b")       'helm-buffers-list)
+(global-set-key (kbd "C-x C-b")     'helm-buffers-list)
+(global-set-key (kbd "C-x C-f")     'helm-find-files)
+(global-set-key (kbd "C-f")         'helm-occur)
+(global-set-key (kbd "C-c C-c")     'helm-colors)
+(global-set-key (kbd "C-c alc")     'helm-calcul-expression)
 
-;; Various keyboard bindings
-(global-set-key (kbd "<end>")   (## (end-of-buffer)))
-(global-set-key (kbd "<home>")  (## (beginning-of-buffer)))
-(global-set-key (kbd "C-s")     (## (save-buffer)))
-(global-set-key (kbd "C-x C-s") (## (isearch-forward)))
 
 
 
+
+
+
+;; maybe this in the future
+;; https://github.com/bbatsov/projectile
+
+;; quick run
+;; run the contents of the buffer
+;; https://github.com/syohex/emacs-quickrun
+;; (require 'quickrun)
+;; (global-set-key (kbd "<f12>") 'quickrun)
+
+;; remove to use helm
+;; (speedbar)
+;; (speedbar-toggle-show-all-files)
+;; (global-set-key (kbd "<f1>") 'speedbar-get-focus)
+;; set common speedbar paths, maybe save some typing
+;;(defun sb-sk (k)
+;;  (setq default-directory k)
+;;  (speedbar-update-contents)
+;;  (speedbar-get-focus)
+;;  )
+;; (global-set-key (kbd "C-c C-1") (lambda () (interactive) (sb-sk "c:/users/analog/Desktop/") ))
+;; (global-set-key (kbd "C-c C-2") (lambda () (interactive) (sb-sk "c:/users/analog/Dropbox/") ))
+
+;; turn capslock into ctrl
+;; install AutoHotkey
+;; Save this to script.ahk
+;; double click to install
+;;
+;; #IfWinActive emacs  ; if in emacs
+;;    +Capslock::Capslock ; make shift+Caps-Lock the Caps Lock toggle
+;;    Capslock::Control   ; make Caps Lock the control button
+;;    #IfWinActive        ; end if in emacs
 
